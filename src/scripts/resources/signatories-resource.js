3739463adb53b3a74fb1e15b8ceeb152ab389989
Rafael Camargo <contato@rafaelcamargo.com>
2017-11-12 02:45:35 -0200

    Avoiding cache when retrieving signatories data

--- src/scripts/resources/signatories-resource.js
+++ src/scripts/resources/signatories-resource.js
@@ -3,7 +3,9 @@ import baseResource from 'resources/base-resource.js'
 const _public = {};
 
 _public.get = () => {
-  return baseResource.get('data/signatories.json');
+  return baseResource.get('data/signatories.json', {
+    v: new Date().getTime()
+  });
 };
 
 export default _public;
