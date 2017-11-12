4e3b20cb237e2c16f32d5f8056bb141588f37cd6
Rafael Camargo <contato@rafaelcamargo.com>
2017-11-12 02:35:02 -0200

    Inserting new google fonts and google analytics

--- environments/dev.js
+++ environments/dev.js
@@ -1,4 +1,7 @@
 window.ENV = {
   "TYPE": "dev",
-  "SITE_BASE_URL": "http://localhost:3000"
+  "SITE_BASE_URL": "http://localhost:3000",
+  "GOOGLE_ANALYTICS": {
+    "ID": "UA-24790056-5"
+  }
 }
--- environments/prod.js
+++ environments/prod.js
@@ -1,4 +1,7 @@
 window.ENV = {
   "TYPE": "prod",
-  "SITE_BASE_URL": "https://theunseriousmanifest.org"
+  "SITE_BASE_URL": "https://theunseriousmanifest.org",
+  "GOOGLE_ANALYTICS": {
+    "ID": "UA-24790056-6"
+  }
 }
